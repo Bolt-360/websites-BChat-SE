a97de1eedba05123c2640e199e831374feacfc13
Michael-Andryeer <andryeerm@gmail.com>
2025-02-19 17:50:27 -0300

    Erro com toast parcialmente corrigido

--- packages/landing/src/containers/SaasAppDark/AppDownload/index.js
+++ packages/landing/src/containers/SaasAppDark/AppDownload/index.js
@@ -1,32 +1,47 @@
+import axios from 'axios';
 import phone from 'common/assets/image/saasAppDark/phone.png';
 import Heading from 'common/components/Heading';
+import Input from 'common/components/Input';
 import NextImage from 'common/components/NextImage';
 import Container from 'common/components/UI/Container';
 import { useState } from 'react';
 import PhoneInput from 'react-phone-number-input';
+import { pt } from 'react-phone-number-input/locale/pt-BR.json';
 import 'react-phone-number-input/style.css';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Section, {
   AppPreview, Content,
   CountryList, Grid, QrCode
 } from './appDownload.style';
-import { pt } from 'react-phone-number-input/locale/pt-BR.json';
-import Input from 'common/components/Input';
-import { toast } from 'react-toastify';
-import axios from 'axios';
 
 
 const AppDownload = () => {
   const [value, setValue] = useState();
-  const [nome, setNome] = useState();
+  const [nome, setNome] = useState('');
+
+  const handleNomeChange = (e) => {
+    if (e && e.target) {
+      setNome(e.target.value);
+    }
+  };
 
   const handleSubmit = async () => {
     try {
+      if (!nome || !value) {
+        toast.error('Por favor, preencha todos os campos!');
+        return;
+      }
+
       const response = await axios.post('https://n8n2.bchat.lat/webhook/fale-conosco', {
         nome,
         telefone: value,
       });
+      
       if (response.status === 200) {
         toast.success('Mensagem enviada com sucesso!');
+        setNome('');
+        setValue('');
       } else {
         toast.error('Erro ao enviar mensagem!');
       }
@@ -45,8 +60,8 @@ const AppDownload = () => {
               <div className="NameInput">
                 <Input 
                   placeholder="João Silva"
-                  onChange={(e) => setNome(e)}
-                  value={nome}
+                  onChange={handleNomeChange}
+                  value={nome || ''}
                 />
               </div>
               <PhoneInput
@@ -70,6 +85,17 @@ const AppDownload = () => {
           </AppPreview>
         </Grid>
       </Container>
+      <ToastContainer 
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Section>
   );
 };
--- packages/landing/src/pages/_app.js
+++ packages/landing/src/pages/_app.js
@@ -1,18 +1,32 @@
-import { NextUIProvider } from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react';
 import 'common/assets/css/flaticon.css';
-import '../containers/CryptoModern/CountDown/timer.css';
 import 'common/assets/css/icon-example-page.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../containers/CryptoModern/CountDown/timer.css';
 // swiper bundle styles
-import 'swiper/css/bundle';
-import 'common/assets/css/react-slick.css';
 import 'common/assets/css/rc-collapse.css';
-import 'rc-collapse/assets/index.css';
 import 'common/assets/css/rc-drawer.css';
+import 'common/assets/css/react-slick.css';
+import 'rc-collapse/assets/index.css';
+import 'swiper/css/bundle';
 
 export default function CustomApp({ Component, pageProps }) {
   return (
       <NextUIProvider>
         <Component {...pageProps} />
+        <ToastContainer 
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark" // tema escuro para combinar com seu design
+        />
       </NextUIProvider>
   );
 }
