d8291e638ac793d0cbc005754467803ffd556839
matheus <matheus@bolt360.com.br>
2025-02-21 13:29:41 -0300

    linguagem alterada

--- packages/landing/src/pages/_document.js
+++ packages/landing/src/pages/_document.js
@@ -27,7 +27,7 @@ export default class CustomDocument extends Document {
   }
   render() {
     return (
-      <Html lang="en">
+      <Html lang="pt-br">
         <Head>
           <link rel="icon" href="/favicon.svg" />
           {/* <link
